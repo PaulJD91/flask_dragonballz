88be370b11d86f435c9c9805cf13e9e05531f44d
paul dennehy <paul.dennehy@hotmail.com>
2023-07-26 12:32:30 +0100

    more set up code added to models

--- models.py
+++ models.py
@@ -10,11 +10,20 @@ class Character(db.Model):
     power_level = db.Column(db.Integer)
     stage_id = db.Column(db.Integer, db.ForeignKey('stages.id'))
 
+    def __repr__(self):
+        return f'<Character {self.id}: {self.name}>'
+    
+    
 class Stage(db.Model):
     __tablename__ = "stages"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     terrain = db.Column(db.String(64))
     weather = db.Column(db.String(64))
+    characters = db.relationship('Character', backref='stage')
+
+    def __repr__(self):
+        return f'<Stage {self.id}: {self.name}>'
+
     
 
