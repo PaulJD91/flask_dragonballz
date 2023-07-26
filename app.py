1a53dd5b8c2c3a08e73dde56842a653fa17acf97
John <ashton.basile@gmail.com>
2023-07-26 12:09:55 +0100

    We set up the database password in an .env file

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+PASSWORD = os.getenv('PASSWORD')
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{PASSWORD}@localhost:5432/dragonballz_app"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def home():
